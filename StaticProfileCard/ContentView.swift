38cf89abda9741091235930331bb1c96e51c5f52
Francisco Casillas <chiccasillas@gmail.com>
2025-02-24 11:44:38 -0600

    Create a card with static properties to create different iterations of the same card

--- StaticProfileCard/ContentView.swift
+++ StaticProfileCard/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            ProfileCardView(name: "Francisco", role: "Software Engineer")
+            ProfileCardView(name: "Mariana", role: "Marketing")
         }
         .padding()
+    }
+}
+
+struct ProfileCardView: View{
+    var name: String
+    var role: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "person.circle.fill")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text(name)
+                .font(.headline)
+            Text(role)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+        .frame(width: 170)
+        .cornerRadius(10)
+        .background(Color.white)
+        .shadow(radius: 5)
     }
 }
 
