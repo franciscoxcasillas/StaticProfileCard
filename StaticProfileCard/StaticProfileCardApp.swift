290a618090db9ccc9bb9b475e860277f786f8fd5
Francisco Casillas <chiccasillas@gmail.com>
2025-02-24 11:25:00 -0600

    Initial Commit

---
+++ StaticProfileCard/StaticProfileCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticProfileCardApp.swift
+//  StaticProfileCard
+//
+//  Created by Francisco Jean on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct StaticProfileCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
